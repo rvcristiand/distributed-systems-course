f454cfd2a7f9245b023095cc81d8cb87919f9b6a
rvc <rvcristiand@unal.edu.co>
2024-09-13 17:41:47 -0500

    Send images over TCP connection (first try)

--- workshops/01-sockets/Client2.py
+++ workshops/01-sockets/Client2.py
@@ -1,21 +1,60 @@
 # client.py
 import socket
+from PIL import Image
+import io
 
-def main():
-    # Aquí se usa la IP pública del servidor
-    server_ip = input("Introduce la IP pública del servidor: ")
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_ip, 12299))
-    
-    while True:
-        message = input("Escribe un mensaje para enviar al servidor (o 'salir' para terminar): ")
-        if message.lower() == 'salir':
-            break
-        client.send(message.encode('utf-8'))
-        response = client.recv(4096).decode('utf-8')
-        print(f"Respuesta del servidor: {response}")
-    
-    client.close()
+
+def image_to_bytes(image_path: str) -> bytes:
+    with Image.open(image_path) as img:
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format=img.format)
+        img_bytes = img_byte_arr.getvalue()
+
+    return img_bytes
+
+def send_bytes(sock, data):
+    total_sent = 0
+    while total_sent < len(data):
+        sent = sock.send(data[total_sent:])
+
+        if sent == 0:
+            raise RuntimeError("La conexión del socket está rota")
+
+        total_sent += sent
+
 
 if __name__ == "__main__":
-    main()
+    import typer
+
+    app = typer.Typer()
+    
+    @app.command()
+    def send_image(server_ip: str, image_path: str, port: int = 12299):
+        """
+        Envia una imagen a través de TCP.
+
+        Parameters
+        ----------
+        server_ip : str
+            IP del servidor al que conectar.
+        image_path : str
+            Ruta de la imagen a enviar.
+        port : int, optional
+            Puerto donde el servidor está escuchando (el valor predeterminado es 12299).
+        """
+        # Aquí se usa la IP pública del servidor
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((server_ip, port))
+
+        img_bytes = image_to_bytes(image_path)
+
+        # enviar el tamaño de la imagen primero (4 bytes)
+        size = len(img_bytes)
+        client.sendall(size.to_bytes(4, 'big'))
+
+        # envia la imagen
+        send_bytes(client, img_bytes)
+
+        client.close()
+
+    app()
--- workshops/01-sockets/Server2.py
+++ workshops/01-sockets/Server2.py
@@ -1,32 +1,85 @@
 # server.py
+import os
 import socket
 import threading
 
-def handle_client(client_socket):
-    while True:
-        # Recibir datos del cliente
-        message = client_socket.recv(1024).decode('utf-8')
-        if not message:
-            break
-        print(f"Recibido: {message}")
+def recv_bytes(sock, size):
+    data = b''
+
+    while len(data) < size:
+        packet = sock.recv(size - len(data))
         
-        # Enviar una respuesta al cliente
-        response = f"Mensaje recibido: {message}"
-        client_socket.send(response.encode('utf-8'))
-    
+        if not packet:
+            raise RuntimeError("La conexión del socket está rota")
+
+        data += packet
+
+    return data
+
+def handle_client(client_socket, output_dir):
+    # recibir el tamaño de la imagen primero (4 bytes)
+    size_data = recv_bytes(client_socket, 4)
+    size = int.from_bytes(size_data, 'big')
+    print(f"Recibiendo imagen de tamaño {size} bytes")
+
+    # recibir la imagen
+    img_bytes = recv_bytes(client_socket, size)
+
+    outout_path = os.path.join(output_dir,
+                               f"imagen_recibida_{address[0]}_{address[1]}_{size}.jpg")
+    with open(outout_path, "wb") as img_file:
+        img_file.write(img_bytes)
+
+    print(f"Imagen guardada en {outout_path}")
+
     client_socket.close()
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 9999))
-    server.listen(5)
-    print("Servidor escuchando en puerto 9999...")
+def start_server(host: str, port: int, output_dir: str = "./"):
+    """
+    Inicia un servidor para recibir imágenes a través de TCP.
+
+    Parameters
+    ----------
+    host : str
+        Dirección IP del servidor.
+    port : int
+        Puerto donde el servidor estará escuchando.
+    output_dir : str
+        Directorio donde se guardará la imagen recibida.
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Servidor escuchando en {host}:{port}...")
 
     while True:
-        client_socket, addr = server.accept()
-        print(f"Conexión establecida con {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_socket, address = server_socket.accept()
+        print(f"Conexión establecida con {address}")
+        
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, output_dir))
         client_handler.start()
 
 if __name__ == "__main__":
-    main()
+    import typer
+    app = typer.Typer()
+
+    @app.command()
+    def run_server(host: str = "0.0.0.0", port: int = 12299):
+        """
+        Inicia el servidor TCP para recibir imágenes.
+
+        Parameters
+        ----------
+        host : str
+            Dirección IP del servidor (por defecto "0.0.0.0" para escuchcar en todas las interfaces).
+        port : int
+            Puerto donde el servidor estará escuchando (por defecto 12299).
+        output_dir : str
+            Directorio donde se guardará la imagen recibida (por defecto "./").
+        """
+        start_server(host, port)
+
+    app()
